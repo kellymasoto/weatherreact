00988e0d52f02ba682fdfb6cb58b51f25d211341
Kelly Masoto <117077367+kellymasoto@users.noreply.github.com>
2024-08-11 21:43:03 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React from "react";
-import weather from "./weather";
-
+import logo from "./logo.svg";
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello from React</h1>
-        <weather city="Paris" />
+        <img src={logo} className="App-logo" alt="logo" />
+        <br />
+        <Weather />
+        <br />
+        <footer>
+          Coded by{" "}
+          <a href="https://github.com/MLZ6" target="_blank" rel="noreferrer">
+            Maitumelo Moaletsi
+          </a>{" "}
+          is{" "}
+          <a
+            href="https://github.com/MLZ6/my-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourced on Github
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://monumental-dasik-fdb48b.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
       </header>
     </div>
   );
