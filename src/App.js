b7c970b2c3005be718d5bfe4f3071c81b0ae7a47
Kelly Masoto <117077367+kellymasoto@users.noreply.github.com>
2024-08-11 22:05:29 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Weather from "./Weather";
+import SearchWeather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <br />
-        <Weather />
-        <br />
-        <footer>
-          Coded by{" "}
-          <a href="https://github.com/MLZ6" target="_blank" rel="noreferrer">
-            Maitumelo Moaletsi
-          </a>{" "}
-          is{" "}
-          <a
-            href="https://github.com/MLZ6/my-react-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Open-sourced on Github
-          </a>{" "}
-          and{" "}
-          <a
-            href="https://monumental-dasik-fdb48b.netlify.app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            hosted on Netlify
-          </a>
-        </footer>
+        <h1>Weather App</h1>
+        <SearchWeather />
       </header>
+      <footer>
+        <a href="https://github.com/lggomez18/weather-react">Open-sourced</a>on
+        Github 💻 <br />
+        Hosted on{" "}
+        <a href="https://weather-react-page.netlify.app/">Netlify ❇️</a>
+      </footer>
     </div>
   );
 }
