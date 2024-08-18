743d8fab2825f90d14dd4c8be7611c19d5ff770c
Kelly Masoto <117077367+kellymasoto@users.noreply.github.com>
2024-08-18 09:00:40 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,33 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather defaultCity="San Francisco" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/kellymasoto"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Kelly Masoto
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/kellymasoto/weatherreact"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://maskel.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
