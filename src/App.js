8bee3b8f3aa8582a6643c63dc551bf49715ed764
Kelly Masoto <117077367+kellymasoto@users.noreply.github.com>
2024-08-18 09:19:54 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ export default function App() {
           </a>{" "}
           and{" "}
           <a
-            href="https://maskel.netlify.app/"
+            href="https://maskel.netlify.app/?"
             target="_blank"
             rel="noopener noreferrer"
           >
