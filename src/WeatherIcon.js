5af83b39f06c254d1c5e7fd78bd4e85bc39462da
Kelly Masoto <117077367+kellymasoto@users.noreply.github.com>
2024-08-18 12:00:10 +0200

    Update WeatherIcon.js

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -2,21 +2,22 @@ import React from "react";
 import ReactAnimatedWeather from "react-animated-weather";
 
 export default function WeatherIcon(props) {
+  console.log(props);
   const codeMapping = {
     "clear-sky-day": "CLEAR_DAY",
     "clear-sky-night": "CLEAR_NIGHT",
     "few-clouds-day": "PARTLY_CLOUDY_DAY",
     "few-clouds-night": "PARTLY_CLOUDY_NIGHT",
-    "scattered-clouds-day": "PARTLY_CLOUDY_DAY",
-    "scattered-clouds-night": "PARTLY_CLOUDY_NIGHT",
+    "scattered-clouds-day": "CLOUDY",
+    "scattered-clouds-night": "CLOUDY",
     "broken-clouds-day": "CLOUDY",
     "broken-clouds-night": "CLOUDY",
     "shower-rain-day": "RAIN",
     "shower-rain-night": "RAIN",
     "rain-day": "RAIN",
     "rain-night": "RAIN",
-    "thunderstorm-day": "RAIN",
-    "thunderstorm-night'": "RAIN",
+    "thunderstorm-day": "SLEET",
+    "thunderstorm-night": "SLEET",
     "snow-day": "SNOW",
     "snow-night": "SNOW",
     "mist-day": "FOG",
@@ -26,8 +27,8 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="#1e1e1e"
-      size={props.size}
+      color="1e1e1e"
+      size={52}
       animate={true}
     />
   );
