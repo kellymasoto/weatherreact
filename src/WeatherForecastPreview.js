2c7390cbe6db76a8991b9e71d488b40602fab33c
Kelly Masoto <117077367+kellymasoto@users.noreply.github.com>
2024-08-18 09:10:54 +0200

    Update WeatherForecastPreview.js

--- src/WeatherForecastPreview.js
+++ src/WeatherForecastPreview.js
@@ -1,33 +1,37 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
-export default function WeatherForecastPreview(props) {
-  function day() {
-    let date = new Date(props.data.time * 1000);
-    let day = date.getDay();
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-    return days[day];
-  }
-
+export default function WeatherForecastDay(props) {
   function maxTemperature() {
-    let temperature = Math.round(props.data.temperature.maximum);
-
+    let temperature = Math.round(props.data.temp.max);
     return `${temperature}°`;
   }
 
   function minTemperature() {
-    let temperature = Math.round(props.data.temperature.minimum);
-
+    let temperature = Math.round(props.data.temp.min);
     return `${temperature}°`;
   }
 
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+
   return (
-    <div className="WeatherForecastPreview">
-      <div className="forecast-time">{day()}</div>
-      <WeatherIcon code={props.data.condition.icon} size={38} />
-      <div className="forecast-temperature">
-        <span className="forecast-temperature-max">{maxTemperature()}</span>
-        <span className="forecast-temperature-min">{minTemperature()}</span>
+    <div>
+      <div className="WeatherForecast-day">{day()}</div>
+      <WeatherIcon code={props.data.weather[0].icon} size={36} />
+      <div className="WeatherForecast-temperatures">
+        <span className="WeatherForecast-temperature-max">
+          {maxTemperature()}
+        </span>
+        <span className="WeatherForecast-temperature-min">
+          {minTemperature()}
+        </span>
       </div>
     </div>
   );
